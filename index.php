46caa4f47f7901ebc78d769151fd93a5b9644ae7
jcutchens <jamescutchens@yahoo.com>
2013-07-17 13:38:39 -0400

    V1
    PHP working not styled

---
+++ index.php
@@ -0,0 +1,266 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>James Cutchens - IMD 410</title>
+<script src="SpryAssets/SpryTabbedPanels.js" type="text/javascript"></script>
+<link href="SpryAssets/SpryTabbedPanels.css" rel="stylesheet" type="text/css">
+<link href="css/style.css" rel="stylesheet" type="text/css">
+<link href='http://fonts.googleapis.com/css?family=Oswald:400,300' rel='stylesheet' type='text/css'>
+</head>
+
+<body>
+	<img src="images/logo.png" width="250" height="248" alt="logo" class="logo">
+	<header>
+    <h2>IMD 410 - Summer 2013 - Bennett <?php echo date('Y'); ?></h2>
+    
+	</header>
+    
+    
+<div id="TabbedPanels1" class="TabbedPanels">
+                  <ul class="TabbedPanelsTabGroup">
+                    <li class="TabbedPanelsTab" tabindex="0">Course Description</li>
+                    <li class="TabbedPanelsTab" tabindex="0">Homework</li>
+                    <li class="TabbedPanelsTab" tabindex="0">Assignments</li>
+                    <li class="TabbedPanelsTab" tabindex="0">Labs</li>
+                    <li class="TabbedPanelsTab" tabindex="0">Study Questions</li>
+                  </ul>
+                  <div class="TabbedPanelsContentGroup">
+                  
+                    <div class="TabbedPanelsContent">
+                    <p>Students apply user-centered design principles, database structures, and server-side scripting to design and develop content for server-based dynamic delivery. The course emphasizes design issues relating to the display of dynamic content on the screen and how that content is updated and delivered from databases.</p> 
+                    <p>Credits: 4 Credits, 6 Hours</p>
+                    <p>Prerequisite:</p>
+                    <p>IMD 315 Interactive Interface Design</p>
+                    
+                    </div>
+                    <div class="TabbedPanelsContent"><h3>Homework:</h3>
+                    <p>
+                      <img src="images/cms-wireframe.png" width="1042" height="390" alt="wireframe" class="photo"> 
+                    </p>
+                    <p>CMS CONCEPT WILL REVOLVE AROUND THE MAN OLYMPIC'S WEBSITE.  THE SITE IS USED MAINLY FOR AN ANNUAL EVENT AND NEEDS TO BE BUILT TO WHERE USER CAN GENERATE THEIR OWN CONTENT.</p>
+
+					<p>AS WELL AS UPDATING THE DESIGN OF THE WEBSITE AND THE FUNCTIONALITY, THE MAIN GOAL IS TO MAKE THE SITE EASY TO ACCESS FOR USERS OF THE ORGANIZATION. </p>
+                    
+                    
+                    </div>
+                    
+                    <div class="TabbedPanelsContent">
+                    
+                    <h3>Assignments:</h3>
+                    <h4>Assignment 2:  Custom Dynamic Website via weekly labs</h4>
+                    <p>Due August 8, 2013</p>
+                    <a href="#">Link to Project Here</a> 
+                    
+                
+                    
+<p>Each week we will be learning about server-side scripting and databases by systematically building a custom database powered website. You will take what we do in each lab, and enhance and design it to be custom to the topic of your choice. You will polish the basic site we make together, into an attractive and complete website.  Simply “skinning” the group work is not adequate to earn a passing grade. Rather, you must take what we do together and build upon it.</p>
+
+<p>Your web site should meet the following criteria:</p>
+
+1.        Use of PHP and MySQL to create, update, and display dynamic content
+2.        Display database content using Ajax
+3.        Use web standards with semantic markup (HTML5 and CSS3)
+4.        Have a secure user authentication system with logout feature
+5.        Use a well organized and logical code structure with clear commenting
+6.        Use a template system with dynamic headings and status indicators
+7.        Dynamic display of the page-last-modified date.
+8.        Provide the config.php file and the username and password for admin section via assignment dropbox.
+9.        Submit your code via gitHub repository
+
+<p>Marking Criteria</p>
+Total Points:  /250
+●         Interaction Design and Usability (50)
+●         Visual Design (100)
+●         Technical Improvements (100)
+
+ 				<h4>Assignment 3:  Dynamic site via CMS platform</h4>
+                <p>Due Date:September 5, 2013</p>
+                <a href="#">Link to Project Here</a>
+
+<p>Using the specified CMS platform, create a website that can be easily updated by the site owner.</p>
+
+<p>Your web site should meet the following criteria:</p>
+
+1.       Display database content using Ajax
+2.       Use web standards with semantic markup (HTML5 and CSS3) for all templates
+3.       Use a well organized and logical code structure with clear commenting
+4.       Have a well organized set of Channels, Channel Fields & Field Groups
+5.       Implement well organized Templates & Template Groups
+6.        
+7.       Effective use of includes/snippets and Global Template Variables to keep things DRY
+8.       Use at least two EE add-ons to add complex functionality to the site
+9.       Provide the username and password for admin section via assignment dropbox.
+
+<p>Marking Criteria</p>
+Total Points:  /250              
+
+●         Design Comps (50)
+●         Interaction Design and Usability (50)
+●         Visual Design (50)
+●         Template code quality (50)
+●         Quality assurance (50)
+                    
+                    
+                    </div>
+                    
+                    <div class="TabbedPanelsContent">
+                    <h3>Labs:</h3>
+                     <h4>Lab 1. Introduction to PHP & GitHub</h4>
+                     <a href="#">Link to Project Here</a>
+<p>Use basic PHP syntax and structure output information to an HTML page. Create a configuration file to control how PHP behaves. Use a PHP function to include content from another page into your PHP page. Use GitHub (git version control GUI & remote host) to add version control to your files.</p>
+
+                    <h4>Lab 2. Template System</h4>
+                    <a href="#">Link to Project Here</a>
+                    <p>Create a PHP template system complete with a dynamic status indication and page heading system. Output the current date and time in your timezone on your template pages. Use an external stylesheet to format your pages. Using your new template system, create a small 4 page web site.</p>
+
+					<h4>Lab 3. Form Processing</h4>
+                    <a href="#">Link to Project Here</a>
+					<p>Create a PHP file to filter user input through HTML forms.</p>
+
+					<h4>Lab 4.  Working With MySQL Databases</h4>
+                    <a href="#">Link to Project Here</a>
+<p>Connect to a database, query a table and retrieve content, looping through it to display it on an HTML page. Use phpMyAdmin to create a database table with a primary key and unique id.</p>
+
+					<h4>Lab 5. Update Records</h4>
+                    <a href="#">Link to Project Here</a>
+<p>Create a page with an HTML form that will allow you to update a record to a database.</p>
+
+					<h4>Lab 6. Setting up the ExpressionEngine CMS</h4>
+                    <a href="#">Link to Project Here</a>
+<p>Install, configure, and setup an empty EE CMS website.</p>
+
+<h4>Lab 7 Add Content, Site structure, and Templates</h4>
+<a href="#">Link to Project Here</a>
+<p>Create a user authentication system to protect your web app from unauthorized users.</p>
+
+<h4>Lab 8. Add-Ons</h4>
+<a href="#">Link to Project Here</a>
+<p>Enhance your site with EE 3rd party modules, plugins, and extensions.</p>
+                    
+                    </div>
+                    
+                    <div class="TabbedPanelsContent">
+                    <h3>Study Questions:</h3>
+                     <h4>Week 1:</h4> 
+
+                        <p><strong>1.  List 3 ways PHP is different in use from JavaScript.</strong></p>
+                            a.  PHP = serverside language & JavaScript = local/browser language</br>
+                            b.  PHP needs to be loaded on a server to test/ JS can be tested without</br>
+                            c.  PHP uses a different syntax and acts more dynamically than JS.
+
+                         <p><strong>2.  List the sequence of events triggered by viewing a PHP page.</strong></p>
+                            a.  PHP gets a request from user and uses a PHP engine to get data from database and then plugs that back into the requested web page/app.
+                            
+						<p><strong>3.  Define the term “middleware”.</strong></p>
+Programming or software that connects processes such as browser and server database together to execute exchanges of data.
+ 
+						<p><strong>4.  Define the term “back-end”.</strong></p>
+Server-side or "back-end" relates to the tasks and software that operate to complete tasks for users that are developed separately from html/css/js.
+
+						<p><strong>5. What is meant by an n-tier web application?</strong>
+A data application that is separated into tiers that are distributed between the client and the server.
+
+						<p><strong>6.  What are “magic quotes”</strong></p>
+Originally a way for beginner users of php to write code securely.  It used special characters that were prefixed with a backslash before being passed on.
+</p>
+						<p><strong>7.  What is revision/version control software?</strong></p>
+These are applications that serve as save points for a project versus saving multiple versions of a project on your computer.  This allows users to be able to access versions of a project remotely and even revert to a prior version due to mistakes or other errors.
+ 
+						<p><strong>8.  List the two most popular version control systems.</strong></p>
+Currently the 2 most popular versions are GitHub and Subversion.
+
+						<p><strong>9.  What is a repository?</strong></p>
+A repository acts like an online folder that stores all committed versions of a project.
+ 
+						<p><strong>10.  What are the steps in submitting changes to git?</strong></p>
+Using a client such as github allows a user to create a repository and commit/submit changes to their project.
+
+						<p><strong>11.  What is GitHub?</strong></p>
+A web-based git version control platform that allows users to establish repositories for their versions of software.
+ 
+						<p><strong>12.  Explain the differences between a local and remote repository.</strong></p>
+A local repository would be similar to  folder that is saved on a user's desktop to contain all versions of a project.  A remote repository would be like a folder saved online and accessible from anywhere through a client such as GitHub.
+
+
+<h4>Week 2</h4>
+1.        List the two things that all PHP files must have to work correctly?
+2.        What is the side-effect of NOT including the above two requirements?
+3.        Be familiar/review core programming concepts. (datatypes, variables, operators, conditionals, loops, functions, and objects)
+4.        What is a PHP page template?
+5.        Write the PHP to include external files into the current document.
+6.        What are PHP Super Globals?
+7.        What are the differences in the use of single quotes vs double quotes?
+8.        List the three parts of a PHP error message.
+9.        List the four types of PHP include statements.
+10.     Write the code to assign the string, “test” to a PHP variable.
+
+
+<h4>Week 3</h4>
+1.        What are regular expressions and how are they used?
+2.        How would you use a conditional to check the success or failure of the mail function?
+3.        What output is generated when $_SERVER[“PHP_SELF”] is echoed to a page?
+4.        How can you use $_GET and $_POST to catch content passed from forms?
+5.        What is URL-encoding?
+6.        How do GET and POST differ?
+7.        What does the register_globals flag do? Why is it so important for security?
+8.        Why are hidden form fields helpful when sending content to a PHP script?
+9.        What are SQL, and MySQL, and what are their roles in dynamic web applications?
+10.     What is phpMyAdmin, and what is it's purpose?
+
+
+
+ 
+<h4>Week 4</h4>
+1.        Define the acronym C.R.U.D. and how does it relate to content management?
+2.        What is SQL?
+3.        What are the steps to insert content into a database?
+4.        What programming structure facilitates displaying a database table’s contents?
+5.        Explain how fields are related to rows, and how rows are related to database tables.
+6.        Write the code to submit a query to a database and to display the requested data.
+7.        Identify the PHP code used to connect to a database.
+8.        Why is it best to keep database connection code included rather than internal?
+9.        Why is a unique primary key important for all MySQL database tables?
+10.     What is a foreign key?
+11.     Create a SQL backup of a MySQL database.
+12.     Display the results of a database query as an HTML table.
+
+<h4>Week 6</h4>
+1.        What is a Content Management System?
+2.        List five popular Content Management Systems.
+3.        List 4 criteria used in selecting the most appropriate CMS for a project.
+4.        List four online resources for developing an ExpressionEngine site.
+5.        Describe the following and how they are related: EE Channels, Fields and Field Groups
+6.        List 4 benefits of using a CMS over handcrafting a website.
+
+<h4>Week 7</h4>
+1.        Describe how these are related: EE Tags, Templates, and Template Groups
+2.        What is a cookie? How do you set and delete a cookie?
+3.        What is a session? How do you set and delete session variables?
+4.        What is user authentication?
+5.        Draw a flowchart showing how a user might log into a Content Management System.
+6.        How could you ensure that users cannot access admin pages without signing in first?
+
+          
+<h4>Week 8</h4>
+1.        Identify the HTML form element needed to upload files to a server.
+2.        What is the $ _FILES and how is it used?
+3.        Describe key information we test before uploading a file with PHP?
+4.        Describe the steps to store (in a database) the title, file name, and comments for a file.
+5.        Describe the steps to dynamically display all files that have been uploaded?
+6.        What security issues must you consider when incorporating a file upload system?
+                    </div>
+                  </div>
+            </div>
+
+                    </div>
+                    
+<footer>
+<p>This interactive media project was created by students for educational purposes at The Art Institute of Atlanta and is in no way intended for commercial gain or as a source of public information.</p>
+</footer>
+
+<script type="text/javascript">
+var TabbedPanels1 = new Spry.Widget.TabbedPanels("TabbedPanels1");
+    </script>
+</body>
+</html>
